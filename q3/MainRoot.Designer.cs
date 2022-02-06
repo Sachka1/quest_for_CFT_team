2c5e68941e067bef0031390a422f836965d25aa0
Александр <deryabin.2019@stud.nstu.ru>
2022-02-06 19:38:25 +0700

    Initial commit

---
+++ q3/MainRoot.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace q3
+{
+    partial class MainRoot
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EditNote = new System.Windows.Forms.Button();
+            this.DeleteNote = new System.Windows.Forms.Button();
+            this.TextNote = new System.Windows.Forms.TextBox();
+            this.SaveNote = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // EditNote
+            // 
+            this.EditNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EditNote.Location = new System.Drawing.Point(225, 23);
+            this.EditNote.Name = "EditNote";
+            this.EditNote.Size = new System.Drawing.Size(176, 57);
+            this.EditNote.TabIndex = 1;
+            this.EditNote.Text = "Редактировать";
+            this.EditNote.UseVisualStyleBackColor = true;
+            this.EditNote.Click += new System.EventHandler(this.EditNote_Click);
+            // 
+            // DeleteNote
+            // 
+            this.DeleteNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DeleteNote.Location = new System.Drawing.Point(431, 23);
+            this.DeleteNote.Name = "DeleteNote";
+            this.DeleteNote.Size = new System.Drawing.Size(188, 57);
+            this.DeleteNote.TabIndex = 3;
+            this.DeleteNote.Text = "Удалить";
+            this.DeleteNote.UseVisualStyleBackColor = true;
+            this.DeleteNote.Click += new System.EventHandler(this.DeleteNote_Click);
+            // 
+            // TextNote
+            // 
+            this.TextNote.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextNote.Location = new System.Drawing.Point(12, 158);
+            this.TextNote.Multiline = true;
+            this.TextNote.Name = "TextNote";
+            this.TextNote.Size = new System.Drawing.Size(607, 469);
+            this.TextNote.TabIndex = 5;
+            // 
+            // SaveNote
+            // 
+            this.SaveNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveNote.Location = new System.Drawing.Point(12, 23);
+            this.SaveNote.Name = "SaveNote";
+            this.SaveNote.Size = new System.Drawing.Size(194, 57);
+            this.SaveNote.TabIndex = 6;
+            this.SaveNote.Text = "Добавить";
+            this.SaveNote.UseVisualStyleBackColor = true;
+            this.SaveNote.Click += new System.EventHandler(this.SaveNote_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Exit.Location = new System.Drawing.Point(396, 638);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(223, 57);
+            this.Exit.TabIndex = 7;
+            this.Exit.Text = "Выйти";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 101);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(305, 32);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.MouseHover += new System.EventHandler(this.comboBox2_MouseHover);
+            // 
+            // MainRoot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 707);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.SaveNote);
+            this.Controls.Add(this.TextNote);
+            this.Controls.Add(this.DeleteNote);
+            this.Controls.Add(this.EditNote);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainRoot";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Заметки для ЦФТ";
+            this.Load += new System.EventHandler(this.MainRoot_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button EditNote;
+        private System.Windows.Forms.Button DeleteNote;
+        private System.Windows.Forms.TextBox TextNote;
+        private System.Windows.Forms.Button SaveNote;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ q3/MainRoot.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace q3
+{
+    public partial class MainRoot : Form
+    {
+        public MainRoot()
+        {
+            InitializeComponent();
+        }
+
+
+        private void EditNote_Click(object sender, EventArgs e) // редактирование
+        {
+            if (File.Exists("Notes/" + comboBox2.Text + ".txt"))
+            {
+                TextNote.Text = File.ReadAllText("Notes/" + comboBox2.Text + ".txt");
+            }
+            else
+                MessageBox.Show("Данная заметка не найдена(");
+
+            // обновление комбобокса
+            comboBox2.Items.Clear();
+            string[] allfiles = Directory.GetFiles("Notes/", "*.txt");
+            foreach (string filename in allfiles)
+            {
+                string str = Path.GetFileName(filename);
+                string v = str.Remove(str.Length - 4);
+                comboBox2.Items.Add(v);
+            }
+
+        }
+
+        private void DeleteNote_Click(object sender, EventArgs e) // удаление 
+        {
+            if (File.Exists("Notes/" + comboBox2.Text + ".txt"))
+            {
+                File.Delete("Notes/" + comboBox2.Text + ".txt");
+                // возвращаем исходные значения для новых записей
+                TextNote.Text = "";
+                comboBox2.Text = "Имя Заметки*";
+            }
+            else
+                MessageBox.Show("Данная заметка не найдена(");
+
+            // обновление комбобокса
+            comboBox2.Items.Clear();
+            string[] allfiles = Directory.GetFiles("Notes/", "*.txt");
+            foreach (string filename in allfiles)
+            {
+                string str = Path.GetFileName(filename); // получение имени файла с расширением 
+                string v = str.Remove(str.Length - 4);  // удаление расширения
+                comboBox2.Items.Add(v);
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e) // выход из приложения
+        {
+            Application.Exit();
+        }
+
+        private void SaveNote_Click(object sender, EventArgs e) // Добавление + сохранение заметки
+        {
+            // проверка на символы недопустимые для названия файла
+            if (comboBox2.Text.Contains('/') || comboBox2.Text.Contains('\\') || 
+                comboBox2.Text.Contains(':') || comboBox2.Text.Contains('*') || 
+                comboBox2.Text.Contains('?') || comboBox2.Text.Contains('<') || 
+                comboBox2.Text.Contains('>') || comboBox2.Text.Contains('|'))
+            {
+                MessageBox.Show("Подберите другое название для заметки");
+            }
+            else
+            {
+                // запись в файл
+                string path = $"Notes/{comboBox2.Text}.txt";
+                string text = TextNote.Text;
+
+                File.WriteAllText(path, text);
+            }
+
+
+            // возвращаем исходные значения для новых записей
+            TextNote.Text = "";
+            comboBox2.Text = "Имя заметки*";
+
+            // обновление комбобокса
+            comboBox2.Items.Clear();
+            string[] allfiles = Directory.GetFiles("Notes/", "*.txt");
+            foreach (string filename in allfiles)
+            {
+                string str = Path.GetFileName(filename);
+                string v = str.Remove(str.Length - 4);
+                comboBox2.Items.Add(v);
+            }
+        }
+
+        private void MainRoot_Load(object sender, EventArgs e) // при загрузке формы
+        {
+            // создание одной простейшей заметки
+            string path = $"Notes/FirstNote.txt";
+            string text = "Привет ЦФТ!!";
+
+            File.WriteAllText(path, text);
+
+            comboBox2.Text = "Имя заметки*";
+
+            // загрузка комбобокса
+            string[] allfiles = Directory.GetFiles("Notes/", "*.txt");
+            foreach (string filename in allfiles)
+            {
+                string str = Path.GetFileName(filename);
+                string v = str.Remove(str.Length - 4);
+                comboBox2.Items.Add(v);
+            }
+        }
+
+        private void comboBox2_MouseHover(object sender, EventArgs e)
+        {
+            comboBox2.Text = "";
+
+            // обновление комбобокса
+            comboBox2.Items.Clear();
+            string[] allfiles = Directory.GetFiles("Notes/", "*.txt");
+            foreach (string filename in allfiles)
+            {
+                string str = Path.GetFileName(filename);
+                string v = str.Remove(str.Length - 4);
+                comboBox2.Items.Add(v);
+            }
+
+        }
+    }
+}
